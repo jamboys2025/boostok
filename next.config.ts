b0c194a79a60cda71e0299d45de39e2de5d684c5
jiakai-17 <111171022+jiakai-17@users.noreply.github.com>
2025-08-29 00:42:18 +0800

    fix packages
    - react to 18
    - tailwind to 3
    - add semi ui

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    transpilePackages: ['@douyinfe/semi-ui', '@douyinfe/semi-icons', '@douyinfe/semi-illustrations'],
 };
 
 export default nextConfig;
